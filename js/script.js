47289924d3bb60652811bac726328b71ab74ac77
berlliyanto <126399467+berlliyanto@users.noreply.github.com>
2023-08-30 20:47:58 +0700

    Add Component 30/08/2023

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Submit Message Us
+
+a
+
+function submitMessage() {
+    let inputName = document.getElementById('input-nama').value;
+    let inputTgl = document.getElementById('input-tgl').value;
+    var inputKelamin = document.querySelector('input[name="jenis-kelamin"]:checked');
+    let selectedKelamin;
+    if (inputKelamin) {
+        selectedKelamin = inputKelamin.value;
+    } else {
+        console.log("Belum Memilih Kelamin");
+    }
+    let inputPesan = document.getElementById('input-pesan').value;
+
+    if (inputName != '' && inputTgl != '' && inputKelamin != undefined && inputPesan != '') {
+        let tglSekarang = document.getElementById('tglSekarang-value');
+        let namaValue = document.getElementById('nama-value');
+        let tglValue = document.getElementById('tglLahir-value');
+        let kelaminValue = document.getElementById('kelamin-value');
+        let pesanValue = document.getElementById('pesan-value');
+
+
+        tglSekarang.innerText = new Date;
+        namaValue.innerText = inputName;
+        tglValue.innerText = inputTgl;
+        kelaminValue.innerText = selectedKelamin;
+        pesanValue.innerText = inputPesan;
+    } else {
+        alert('Semua filed harus diisi');
+    }
+    console.log('ee');
+}
